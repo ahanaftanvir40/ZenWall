561d7ceed5728b3507c1aac5c7459ab69e5f9c17
Ahanaf Tanvir <ahanaf.tanvir@g.bracu.ac.bd>
2024-10-25 00:04:01 +0600

    implemented parallexview in explore page

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,21 +1,22 @@
 
-import { View, Text, Button } from 'react-native'
+import { View, Text, Button, Image } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { useState } from 'react'
 import BottomPull from '@/components/BottomPull'
+import ParallaxScrollView from '@/components/ParallexScrollView'
 
 function explore() {
   const [openPicture, setOpenPicture] = useState(false)
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ flex: 1 }}>
+      <ParallaxScrollView headerBackgroundColor={{ dark: 'black', light: 'white' }} headerImage={<Image style={{ flex: 1 }} source={{ uri: "https://ideogram.ai/assets/image/lossless/response/LlzCI1y_S2ihwxDeb6Wz7A" }} />}>
         <Text>Explore Page</Text>
         <Button title='Open BottomSheet' onPress={() => setOpenPicture(true)} />
         {openPicture && (
           <BottomPull onClose={() => setOpenPicture(false)} />
         )}
-      </View>
-    </SafeAreaView>
+      </ParallaxScrollView>
+    </SafeAreaView >
   )
 }
 
